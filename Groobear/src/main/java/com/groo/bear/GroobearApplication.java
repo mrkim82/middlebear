45086681b04c673227a93a3c10b267167fbb962e
user <user@DESKTOP-EUD767P>
2023-07-07 14:42:13 +0900

    그루베어 프로젝트 최초 등록
    그루베어 프로젝트 초기 셋팅 및 layout설정

---
+++ Groobear/src/main/java/com/groo/bear/GroobearApplication.java
@@ -0,0 +1,15 @@
+package com.groo.bear;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan(basePackages = "com.groo.bear.**.mapper")
+public class GroobearApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroobearApplication.class, args);
+	}
+
+}
---
+++ Groobear/src/test/java/com/groo/bear/GroobearApplicationTests.java
@@ -0,0 +1,13 @@
+package com.groo.bear;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroobearApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
