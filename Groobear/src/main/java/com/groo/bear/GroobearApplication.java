a151e506a1ae9fbe9c29f1b96aeb36b9df6827dc
mrkim82 <mrkim82@nate.com>
2023-07-10 12:33:55 +0900

    ..

--- Groobear/src/main/java/com/groo/bear/GroobearApplication.java
+++ Groobear/src/main/java/com/groo/bear/GroobearApplication.java
@@ -3,11 +3,12 @@ package com.groo.bear;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 @MapperScan(basePackages = "com.groo.bear.**.mapper")
 public class GroobearApplication {
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(GroobearApplication.class, args);
 	}
---
+++ Groobear/src/main/java/com/groo/bear/emp/mapper/EmpMapper.java
@@ -0,0 +1,8 @@
+package com.groo.bear.emp.mapper;
+
+import com.groo.bear.emp.service.EmpVO;
+
+public interface EmpMapper {
+	//로그인
+	public EmpVO login(EmpVO vo);
+}
