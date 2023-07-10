9b67d35bf63a35b39fb5774c57e265bc3cfe1a21
test01 <servel0344@naver.com>
2023-07-10 21:48:03 +0900

    프로젝트 즐겨찾기
    템플릿 오류, 신규 생성 제외

--- Groobear/src/main/java/com/groo/bear/pro/controller/ProController.java
+++ Groobear/src/main/java/com/groo/bear/pro/controller/ProController.java
@@ -12,6 +12,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.groo.bear.pro.service.ProService;
 import com.groo.bear.pro.service.ProVO;
@@ -26,7 +27,7 @@ public class ProController {
 	@GetMapping("proMain")
 	public String proMainPage(Model model, HttpServletRequest request) {
 		HttpSession session = request.getSession();
-		model.addAttribute("projectMainList", proService.readProject((String)session.getAttribute("UserId")));
+		model.addAttribute("projectMainList", proService.readProject((String)session.getAttribute("Id")));
 		return "pro/proMain";
 	}
 	
@@ -53,7 +54,7 @@ public class ProController {
 		param.put("v_post_view_auth", proVO.getPostViewAuth());
 		param.put("v_com_write_auth", proVO.getComWriteAuth());
 		param.put("v_file_auth", proVO.getFileAuth());
-		param.put("v_id", (String)session.getAttribute("UserId"));
+		param.put("v_id", (String)session.getAttribute("Id"));
 		
 		proService.insertPro(param);
 		System.out.println("param 결과 : " + param.toString());
@@ -61,10 +62,18 @@ public class ProController {
 	}
 	
 //	//프로젝트 즐겨찾기
-//	@PutMapping("")
-//	public void starCheck() {
-//		//프로젝트 즐겨찾기 등록
-//		
-//		//프로젝트 즐겨찾기 취소
-//	}
+	@PostMapping("proUpdate")
+	public String starCheck(@RequestParam("pMN") int pMN, @RequestParam("starC") String starC) {
+		String res;
+		if(starC.equals("N")) {
+			//프로젝트 즐겨찾기 등록
+			proService.updateStarY(pMN);
+			res = "등록";
+		} else{
+			//프로젝트 즐겨찾기 취소
+			proService.updateStarN(pMN);
+			res = "취소";
+		}
+		return res;
+	}
 }
