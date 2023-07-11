0b0d993770a05424635a1d2853ccc46479f70588
mrkim82 <mrkim82@nate.com>
2023-07-11 12:45:05 +0900

    프로젝트 합침

--- Groobear/src/main/java/com/groo/bear/board/controller/BoardController.java
+++ Groobear/src/main/java/com/groo/bear/board/controller/BoardController.java
@@ -1,6 +1,5 @@
 package com.groo.bear.board.controller;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,34 +12,36 @@ import com.groo.bear.board.service.BoardVO;
 
 @Controller
 public class BoardController {
-	
+
 	@Autowired
 	BoardService boardService;
-	
-	//전체조회페이지
+
+	// 전체조회페이지
 	@GetMapping("boardList")
 	public String boardList(Model model) {
 		System.out.println(boardService.selectAllList());
 		model.addAttribute("boardList", boardService.selectAllList());
 		return "board/boardList";
 	}
-	
-	//단건조회페이지
+
+	// 단건조회페이지
 	@GetMapping("getBoard")
 	public String getBoard(Model model, @RequestParam int bNo) {
 		model.addAttribute("getBoard", boardService.selectBoard(bNo));
 		return "board/getBoard";
 	}
-	
-	//등록 페이지
+
+	// 등록 페이지
 	@GetMapping("boardInsert")
 	public String boardInsertForm(Model model) {
-		model.addAttribute("BoardVO", new BoardVO());
-		//if()
+		
+		 model.addAttribute("BoardVO", new BoardVO()); System.out.println(model);
+		 //if()
+		 
 		return "board/boardInsert";
 	}
-	
-	//등록 처리
+
+	// 등록 처리
 	@PostMapping("boardInsert")
 	public String boardInsert(BoardVO vo) {
 		boardService.insertBoard(vo);
