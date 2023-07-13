8472e713e5e4df301faab0e0d5530a3bce97a501
dohyun <wook118@naver.com>
2023-07-13 17:48:41 +0900

    그루베어 프로잭트
    0713

--- Groobear/src/main/java/com/groo/bear/mypage/service/CarService.java
+++ Groobear/src/main/java/com/groo/bear/mypage/service/CarService.java
@@ -1,14 +1,20 @@
 package com.groo.bear.mypage.service;
 
 import java.util.List;
+import java.util.Map;
+
+import com.groo.bear.mypage.paging.Criteria;
 
 public interface CarService {
 
 	//전체 조회
 	public List<CarVO> getAllCarList();
+	public List<CarVO> getAllCarList(CarVO carVO);
 	//차량 개별조
 	public List<CarVO> getMyCarList(String id);
 	
-	
+	//페이징
+	public int carListCnt() throws Exception;
+	public List<Map<String, Object>> carList(Criteria cri) throws Exception;
 	
 }
