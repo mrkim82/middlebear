5121b352cbab0e9834e80233c58fb4dc8ce35451
umjooonsick <138838792+umjooonsick@users.noreply.github.com>
2023-07-14 00:11:36 +0900

    Gwan (#16)
    * 그루베어 깃 테스트
    
    * 프로젝트 붙여넣기
    
    * 프로젝트 생성 테스트 완료
    
    권한 추가 할 것
    
    * 프로젝트
    
    main조금 수정, index파일 원상복구
    
    * 프로젝트 생성(프로시저)
    
    프로시저 이용한 java단 수정
    
    * 프로젝트 프로시저 수정
    
    java부분 수정
    
    * 프로젝트 생성 프로시저(수정)
    
    Mapper.xml 오류 수정
    varchar2 타입 사용 불가
    
    * 프로젝트 생성 완료
    
    프로젝트 생성 프로시저 완료
    7.10 12:13 java.sql.SQLIntegrityConstraintViolationException과 ORA-00001:
    unique constraint (BEAR.XPK프로젝트) violated 관련 오류 뜸
    
    * 프로젝트
    
    자잘한 내용 수정
    
    * 프로젝트 학원
    
    * 프로젝트 즐겨찾기
    
    템플릿 오류, 신규 생성 제외
    
    * 프로젝트 메인
    
    각종 필터
    
    * 프로젝트 메인
    
    프로젝트 그룹 상세 조회 완료
    
    * 프로젝트
    
    즐겨찾기 이동 성공
    
    * 프로젝트 중간저장
    
    점심전
    
    * 프로젝트 사이드바
    
    프로젝트 사이드바 관리
    
    * 강병관 중간저장
    
    학원 종료
    
    * 조금
    
    * 프로젝트 메인
    
    미분류 진행중, 그룹 상세 리스트 페이지 수정, 프로젝트 그룹 번호 받아오기...
    
    * 프로젝트
    
    페이지 오류 수정
    
    * 프로젝트 사이드바 코드분리
    
    * 프로젝트 사이드바
    
    코드 분리 1차 완료, 예외처리 1차 완료
    
    * 프로젝트 메인
    
    한 것
     - 쿼리문 수정
    할 것
     - 즐겨찾기 문구 css 확인, 정렬, 필터, 설정 추가예정
    
    * 프로젝트
    
    오류수정
    
    * 프로젝트 조회 수정전
    
    조회 파라미터 map으로 바꾸기전 저장
    
    * 프로젝트 사이드바
    
    코드정리 및 그룹 삭제 완료
    
    * 프로젝트 사이드바
    
    프로젝트 폴더 + 후 window로 모달창 안꺼지는 상황
    그룹명 수정 A그룹 갔다가 B그룹 가서 수정하면 그룹명 동시변경
    삭제완료
    모달창4번 none작업
    모달창 1 윈도우 안닫김
    esc로 모달창 닫기
    
    * 합병
    
    * 프로젝트 메인
    
    오류 수정 및 페이지 이동
    
    * 강병관 프로젝트 학원
    
    * 프로젝트 메인
    
    모달창 수정
    
    * 프로젝트 오류 테스트
    
    * 프로젝트 오류 수정
    
    ---------
    
    Co-authored-by: test01 <servel0344@naver.com>
    Co-authored-by: user <user@DESKTOP-D8NAJEC>
    Co-authored-by: serve <serve@sung>

---
+++ Groobear/src/main/java/com/groo/bear/pro/service/impl/ProPostServiceImpl.java
@@ -0,0 +1,22 @@
+package com.groo.bear.pro.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.groo.bear.pro.mapper.ProPostMapper;
+import com.groo.bear.pro.service.ProPostService;
+import com.groo.bear.pro.service.ProPostVO;
+
+@Service
+public class ProPostServiceImpl implements ProPostService {
+	@Autowired
+	ProPostMapper ppm;
+	
+	@Override
+	public List<ProPostVO> readTopMenu(int proMemNo) {
+		return ppm.readTopMenu(proMemNo);
+	}
+
+}
