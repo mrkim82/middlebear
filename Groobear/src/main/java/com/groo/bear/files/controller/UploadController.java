1d5180f85217f728d55daea8ae5eaa2a1a2b2990
김욱환 <lawyeruh@naver.com>
2023-07-17 17:27:57 +0900

    17:27 김욱환

--- Groobear/src/main/java/com/groo/bear/files/controller/UploadController.java
+++ Groobear/src/main/java/com/groo/bear/files/controller/UploadController.java
@@ -1,17 +1,33 @@
 package com.groo.bear.files.controller;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+import java.util.UUID;
 
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.groo.bear.files.domain.FilesVO;
+
 import lombok.extern.log4j.Log4j2;
+import net.coobird.thumbnailator.Thumbnailator;
 
 @Controller
 @Log4j2
@@ -34,42 +50,111 @@ public class UploadController {
 		
 	}
 	
-	@PostMapping("/uploadAjaxAction")
+	@PostMapping(value = "/uploadAjaxAction", produces = MediaType.APPLICATION_JSON_VALUE)
 	@ResponseBody
-	public void uploadAjaxPost(MultipartFile[] uploadFile) {
+	public ResponseEntity<List<FilesVO>> uploadAjaxPost(MultipartFile[] uploadFile) {
+		List<FilesVO> list = new ArrayList<>();
+		
 		log.info("update ajax post ......");
 		
 		String uploadFolder = "C:\\upload";
 		
-		//make folder ----------
-		File uploadPath = new File(uploadFolder, getFolder());
-		log.info("upload path: " + uploadPath);
+		String uploadFolderPath = getFolder();
 		
-		if(uploadPath.exists() == false) {
-			uploadPath.mkdirs();
-		}
-		//make yyyy/MM/dd folder
-		
+		  //make folder ---------- 
+		  File uploadPath = new File(uploadFolder,getFolder()); 
+		  //log.info("upload path: " + uploadPath);
+		  
+		  if(uploadPath.exists() == false) { 
+			  uploadPath.mkdirs(); 
+		  } 
+		  //make yyyy/MM/dd folder
+		 		
 		for(MultipartFile multipartFile : uploadFile) {
 			
-			log.info("--------------------------------------");
-			log.info("Upload File Name : " + multipartFile.getOriginalFilename());
-			log.info("Upload File Size : " + multipartFile.getSize());
+			FilesVO filesVO = new FilesVO();
+			
+//			log.info("--------------------------------------");
+//			log.info("Upload File Name : " + multipartFile.getOriginalFilename());
+//			log.info("Upload File Size : " + multipartFile.getSize());
 			
 			String uploadFileName = multipartFile.getOriginalFilename();
 			
 			//IE has file path
 			uploadFileName = uploadFileName.substring(uploadFileName.lastIndexOf("\\") + 1);
 			log.info("only file name: " + uploadFileName);
+			filesVO.setFileName(uploadFileName);
 			
-			//File saveFile = new File(uploadFolder, uploadFileName);
-			File saveFile = new File(uploadPath, uploadFileName);
+			UUID uuid = UUID.randomUUID();
+			uploadFileName = uuid.toString() + "_" + uploadFileName;
 			
 			try {
+				File saveFile = new File(uploadPath, uploadFileName);
 				multipartFile.transferTo(saveFile);
+				filesVO.setUuid(uuid.toString());
+				filesVO.setUploadPath(uploadFolderPath);
+				
+				//check image type file
+				if(checkImageType(saveFile)) {
+					filesVO.setImage(true);
+					FileOutputStream thumbnail = new FileOutputStream(new File(uploadPath, "s_" + uploadFileName));
+					
+					Thumbnailator.createThumbnail(new FileInputStream(saveFile), thumbnail, 100, 100);
+					thumbnail.close();
+				}
+				//add to List
+				list.add(filesVO);
+				
 			} catch (Exception e) {
-				log.error(e.getMessage());
+				//log.error(e.getMessage());
+				e.printStackTrace();
 			} //end catch
 		} // end for
+		return new ResponseEntity<>(list, HttpStatus.OK);
+	}
+	
+	private boolean checkImageType(File file) {
+		
+		try {
+			String contentType = Files.probeContentType(file.toPath());
+			
+			return contentType.startsWith("image");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return false;
+	}
+	
+	@GetMapping("/display")
+	@ResponseBody
+	public ResponseEntity<byte[]> getFile(String fileName) {
+		log.info("fileName : " + fileName);
+		File file = new File("c:\\upload\\" + fileName);
+		log.info("file: " + file);
+		
+		ResponseEntity<byte[]> result = null;
+		
+		try {
+			HttpHeaders header = new HttpHeaders();
+			
+			header.add("Content-Type", Files.probeContentType(file.toPath()));
+			result = new ResponseEntity<>(FileCopyUtils.copyToByteArray(file), header, HttpStatus.OK);
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return result;
+	}
+	
+	@GetMapping(value = "/download", produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
+	@ResponseBody
+	public ResponseEntity<Resource> downloadFile(String fileName) {
+		System.out.println("1");
+		log.info("download file: " + fileName);
+		
+		FileSystemResource resource = new FileSystemResource("C:\\upload\\" + fileName);
+		
+		log.info("resource : " + resource);
+		return null;
 	}
 }
--- Groobear/src/main/java/com/groo/bear/files/domain/FilesVO.java
+++ Groobear/src/main/java/com/groo/bear/files/domain/FilesVO.java
@@ -4,10 +4,22 @@ import lombok.Data;
 
 @Data
 public class FilesVO {
+	
+//	FILE_NO          NOT NULL NUMBER(5)    
+//	ORIGIN_FILE_NAME NOT NULL VARCHAR2(50) 
+//	CHANGE_FILE_NAME NOT NULL VARCHAR2(50) 
+//	FILE_SIZE        NOT NULL NUMBER(10)   
+//	FILE_DATE        NOT NULL DATE         
+//	MAIL_NO                   NUMBER(5)    
+//	BOARD_NO                  NUMBER(5)    
+//	PRO_FILE_NO               NUMBER(5)
+	
 	private String uuid;
 	private String uploadPath;
 	private String fileName;
-	private boolean fileType;
+	private boolean image;
 	
 	private int boardNo;
+	private int mailNo;
+	private int proFileNo;
 }
