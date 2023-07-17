629efdee5a31f9cc0c195507d4f005cb91536933
umjooonsick <138838792+umjooonsick@users.noreply.github.com>
2023-07-17 11:46:20 +0900

    Devgwan (#28)
    * 그루베어 깃 테스트
    
    * 프로젝트 붙여넣기
    
    * 프로젝트 생성 테스트 완료
    
    권한 추가 할 것
    
    * 프로젝트
    
    main조금 수정, index파일 원상복구
    
    * 프로젝트 생성(프로시저)
    
    프로시저 이용한 java단 수정
    
    * 프로젝트 프로시저 수정
    
    java부분 수정
    
    * 프로젝트 생성 프로시저(수정)
    
    Mapper.xml 오류 수정
    varchar2 타입 사용 불가
    
    * 프로젝트 생성 완료
    
    프로젝트 생성 프로시저 완료
    7.10 12:13 java.sql.SQLIntegrityConstraintViolationException과 ORA-00001:
    unique constraint (BEAR.XPK프로젝트) violated 관련 오류 뜸
    
    * 프로젝트
    
    자잘한 내용 수정
    
    * 프로젝트 학원
    
    * 프로젝트 즐겨찾기
    
    템플릿 오류, 신규 생성 제외
    
    * 프로젝트 메인
    
    각종 필터
    
    * 프로젝트 메인
    
    프로젝트 그룹 상세 조회 완료
    
    * 프로젝트
    
    즐겨찾기 이동 성공
    
    * 프로젝트 중간저장
    
    점심전
    
    * 프로젝트 사이드바
    
    프로젝트 사이드바 관리
    
    * 강병관 중간저장
    
    학원 종료
    
    * 조금
    
    * 프로젝트 메인
    
    미분류 진행중, 그룹 상세 리스트 페이지 수정, 프로젝트 그룹 번호 받아오기...
    
    * 프로젝트
    
    페이지 오류 수정
    
    * 프로젝트 사이드바 코드분리
    
    * 프로젝트 사이드바
    
    코드 분리 1차 완료, 예외처리 1차 완료
    
    * 프로젝트 메인
    
    한 것
     - 쿼리문 수정
    할 것
     - 즐겨찾기 문구 css 확인, 정렬, 필터, 설정 추가예정
    
    * 프로젝트
    
    오류수정
    
    * 프로젝트 조회 수정전
    
    조회 파라미터 map으로 바꾸기전 저장
    
    * 프로젝트 사이드바
    
    코드정리 및 그룹 삭제 완료
    
    * 프로젝트 사이드바
    
    프로젝트 폴더 + 후 window로 모달창 안꺼지는 상황
    그룹명 수정 A그룹 갔다가 B그룹 가서 수정하면 그룹명 동시변경
    삭제완료
    모달창4번 none작업
    모달창 1 윈도우 안닫김
    esc로 모달창 닫기
    
    * 합병
    
    * 프로젝트 메인
    
    오류 수정 및 페이지 이동
    
    * 강병관 프로젝트 학원
    
    * 프로젝트 메인
    
    모달창 수정
    
    * 프로젝트 오류 테스트
    
    * 프로젝트 오류 수정
    
    * 프로젝트 메인
    
    거의 완료
    
    * 프로젝트 수정
    
    * 찐
    
    * 프로젝트 메인 필터 수정
    
    * 프로젝트 경로 수정
    
    * 피드 중간저장
    
    * 토요일저장
    
    * 글 생성 전 저장
    
    * 22
    
    * 중간저장
    
    * 업무 등록 중간저장
    
    * 매퍼 배열 진행중
    
    * ..
    
    * ..
    
    * 7.17 강병관 마이바티스 핸들러타입지정
    
    ---------
    
    Co-authored-by: test01 <servel0344@naver.com>
    Co-authored-by: user <user@DESKTOP-D8NAJEC>
    Co-authored-by: serve <serve@sung>

--- Groobear/src/main/java/com/groo/bear/board/controller/BoardController.java
+++ Groobear/src/main/java/com/groo/bear/board/controller/BoardController.java
@@ -21,86 +21,86 @@ import com.groo.bear.paging.Paging;
 
 @Controller
 public class BoardController {
-	
-	@Autowired
-	BoardService boardService;
-	
-	@GetMapping("/boardList")
-	public String getboardList(Criteria cri, Model model, BoardVO boardVO) {
-		// 전체 글 개수
+   
+   @Autowired
+   BoardService boardService;
+   
+   @GetMapping("/boardList")
+   public String getboardList(Criteria cri, Model model, BoardVO boardVO) {
+      // 전체 글 개수
         int boardListCnt = boardService.boardListCnt(cri, boardVO);
         
         // 페이징 객체
         Paging paging = new Paging();
         paging.setCri(cri);
         paging.setTotalCount(boardListCnt);
-		
-		model.addAttribute("boardList", boardService.selectAllList(cri, boardVO));
-		model.addAttribute("paging", paging);
-		
-		return "board/boardList";
-	}
-//	@GetMapping("/uploadAjax")
-//	public void uploadAjax() {
-//		log.info("upload ajax");
-//	}
-	
-	@GetMapping("boardInfo")
-	public String getBoard(Model model, @RequestParam int boardNo) {
-		model.addAttribute("board", boardService.selectBoard(boardNo));
-		return "board/boardInfo";
-	}
-	
-	//등록 페이지
-	@GetMapping("boardInsert")
-	public String boardInsertForm(Model model, BoardVO vo) {
-		System.out.println(vo);
-		model.addAttribute("board", vo);
-		return "board/boardInsert";
-	}
-	
-	@PostMapping("boardInsert")
-	public String boardInsert(@ModelAttribute BoardVO boardVO, Model model) {
-		boardService.insertBoard(boardVO);
-		return "redirect:/boardList?boardType=" + boardVO.getBoardType();
-	}
-	
-	@GetMapping("boardUpdate/{boardNo}")
-	public String getUpdatePage(@PathVariable("boardNo") int boardNo, Model model) {
-	    BoardVO board = boardService.selectBoard(boardNo);
-	    model.addAttribute("board", board);
-	    return "board/boardUpdate";
-	}
+      
+      model.addAttribute("boardList", boardService.selectAllList(cri, boardVO));
+      model.addAttribute("paging", paging);
+      
+      return "board/boardList";
+   }
+//   @GetMapping("/uploadAjax")
+//   public void uploadAjax() {
+//      log.info("upload ajax");
+//   }
+   
+   @GetMapping("boardInfo")
+   public String getBoard(Model model, @RequestParam int boardNo) {
+      model.addAttribute("board", boardService.selectBoard(boardNo));
+      return "board/boardInfo";
+   }
+   
+   //등록 페이지
+   @GetMapping("boardInsert")
+   public String boardInsertForm(Model model, BoardVO vo) {
+      System.out.println(vo);
+      model.addAttribute("board", vo);
+      return "board/boardInsert";
+   }
+   
+   @PostMapping("boardInsert")
+   public String boardInsert(@ModelAttribute BoardVO boardVO, Model model) {
+      boardService.insertBoard(boardVO);
+      return "redirect:/boardList?boardType=" + boardVO.getBoardType();
+   }
+   
+   @GetMapping("boardUpdate/{boardNo}")
+   public String getUpdatePage(@PathVariable("boardNo") int boardNo, Model model) {
+       BoardVO board = boardService.selectBoard(boardNo);
+       model.addAttribute("board", board);
+       return "board/boardUpdate";
+   }
 
-	@PostMapping("boardUpdate")
-	public String boardUpdate(BoardVO boardVO, Model model) {
-		System.out.println(boardVO);
-		boardService.updateBoard(boardVO);
-		return "redirect:/boardInfo?boardNo=" + boardVO.getBoardNo();
-	}
-	
-	@DeleteMapping("boardDelete/{boardNo}")
-	@ResponseStatus(HttpStatus.OK)
-	@ResponseBody
-	public String boardDelete(@PathVariable("boardNo") int boardNo) {
-	    boardService.deleteBoard(boardNo);
-	    return "게시글이 삭제되었습니다.";
-	}
-	
-	/*
-	 * @RequestMapping(value="boardList") public String boardList(Criteria cri,
-	 * Model model) throws Exception {
-	 * 
-	 * // 전체 글 개수 int boardListCnt = boardService.boardListCnt();
-	 * 
-	 * // 페이징 객체 Paging paging = new Paging(); paging.setCri(cri);
-	 * paging.setTotalCount(boardListCnt);
-	 * 
-	 * List<Map<String, Object>> list = boardService.boardList(cri);
-	 * 
-	 * model.addAttribute("list", list); model.addAttribute("paging", paging);
-	 * 
-	 * return "board/boardList"; }
-	 */
-	
+   @PostMapping("boardUpdate")
+   public String boardUpdate(BoardVO boardVO, Model model) {
+      System.out.println(boardVO);
+      boardService.updateBoard(boardVO);
+      return "redirect:/boardInfo?boardNo=" + boardVO.getBoardNo();
+   }
+   
+   @DeleteMapping("boardDelete/{boardNo}")
+   @ResponseStatus(HttpStatus.OK)
+   @ResponseBody
+   public String boardDelete(@PathVariable("boardNo") int boardNo) {
+       boardService.deleteBoard(boardNo);
+       return "게시글이 삭제되었습니다.";
+   }
+   
+   /*
+    * @RequestMapping(value="boardList") public String boardList(Criteria cri,
+    * Model model) throws Exception {
+    * 
+    * // 전체 글 개수 int boardListCnt = boardService.boardListCnt();
+    * 
+    * // 페이징 객체 Paging paging = new Paging(); paging.setCri(cri);
+    * paging.setTotalCount(boardListCnt);
+    * 
+    * List<Map<String, Object>> list = boardService.boardList(cri);
+    * 
+    * model.addAttribute("list", list); model.addAttribute("paging", paging);
+    * 
+    * return "board/boardList"; }
+    */
+   
 }
--- Groobear/src/main/java/com/groo/bear/pro/mapper/ProPostMapper.java
+++ Groobear/src/main/java/com/groo/bear/pro/mapper/ProPostMapper.java
@@ -4,6 +4,9 @@ import java.util.List;
 
 import com.groo.bear.pro.service.ProPostUserVO;
 import com.groo.bear.pro.service.ProPostVO;
+import com.groo.bear.pro.service.postvo.ProPostWorkGroupVO;
+import com.groo.bear.pro.service.postvo.ProPostWorkVO;
+import com.groo.bear.pro.service.postvo.ProPostWritingVO;
 
 public interface ProPostMapper {
 	//상단메뉴바
@@ -15,4 +18,12 @@ public interface ProPostMapper {
 	
 	// 프로젝트 참가자 정보
 	public List<ProPostUserVO> readProjectParti(ProPostUserVO vo);
+	
+	//글 작성
+	public void createPostWriting(ProPostWritingVO vo);
+	
+	//업무 작성
+	public void createPostWork(ProPostWorkVO vo);
+	//업무 그룹조회
+	public List<ProPostWorkGroupVO> readWritingWorkGroup(int proNo);
 }
