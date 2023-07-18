665f73e17ab8b17dbf01eda670d959d055b38e3a
test01 <servel0344@naver.com>
2023-07-19 00:33:03 +0900

    프로젝트 일정 참여 개선

--- Groobear/src/main/java/com/groo/bear/pro/mapper/ProPostSchMapper.java
+++ Groobear/src/main/java/com/groo/bear/pro/mapper/ProPostSchMapper.java
@@ -7,4 +7,7 @@ import com.groo.bear.pro.service.schvo.ProPostSchVO;
 public interface ProPostSchMapper {
 	//참석자 조회
 	public List<ProPostSchVO> readSchparti(String id);
+	
+	//참석여부 변경
+	public int updateSchPartiCheck(ProPostSchVO vo);
 }
---
+++ Groobear/src/main/java/com/groo/bear/pro/service/impl/ProPostSchServiceImpl.java
@@ -0,0 +1,27 @@
+package com.groo.bear.pro.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.groo.bear.pro.mapper.ProPostSchMapper;
+import com.groo.bear.pro.service.ProPostSchService;
+import com.groo.bear.pro.service.schvo.ProPostSchVO;
+
+@Service
+public class ProPostSchServiceImpl implements ProPostSchService {
+	@Autowired
+	ProPostSchMapper ppsm;
+	
+	@Override
+	public List<ProPostSchVO> readSchparti(String id) {
+		return ppsm.readSchparti(id);
+	}
+
+	@Override
+	public int updateSchPartiCheck(ProPostSchVO vo) {
+		return ppsm.updateSchPartiCheck(vo);
+	}
+
+}
