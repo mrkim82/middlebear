098c7a2c0a610e4e4c118e181918ea76802fe38c
user <user@DESKTOP-D8NAJEC>
2023-07-24 19:22:02 +0900

    풀캘린더 진행중

--- Groobear/src/main/java/com/groo/bear/pro/controller/ProPostSchController.java
+++ Groobear/src/main/java/com/groo/bear/pro/controller/ProPostSchController.java
@@ -18,7 +18,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.groo.bear.pro.service.ProPostSchService;
-import com.groo.bear.pro.service.ProUsersVO;
 import com.groo.bear.pro.service.schvo.ProPostSchVO;
 
 @Controller
